ac092f0a284165f58d813f614bc68aa181097cbc
Tan Duong <dntan09@gmail.com>
2015-09-26 11:56:23 +0700

    Add missing relationships

--- app/models/check_point.rb
+++ app/models/check_point.rb
@@ -1,2 +1,4 @@
 class CheckPoint < ActiveRecord::Base
+  belongs_to :company
+  has_many :questions, dependent: :destroy
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ActiveRecord::Base
+  has_many :users, dependent: :destroy
+  has_many :check_points, dependent: :destroy
+  has_many :mentees, dependent: :destroy
 end
