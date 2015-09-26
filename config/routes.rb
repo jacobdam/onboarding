c41e94e5d97833dfce3e1fd2a4bdf08a0f09d149
Hieu Nguyen <hieuk09@gmail.com>
2015-09-26 14:01:53 +0700

    [f] Customize invitation to add company to mentor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { invitations: 'users/invitations' }
   root to: 'homes#index'
   resource :company, only: %i{new create edit update}
   resource :dashboard, only: %i{show}
