2c4052edca4c81984e50e97d27e0c302d8512f02
Tan Duong <dntan09@gmail.com>
2015-09-26 11:57:15 +0700

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+MAX_COMPANIES = 3
+MAX_EMPLOYEES = 5
+MAX_QUESTIONS = 12
+MAX_CHECK_POINTS = 10
+MAX_MENTEES = 10
+
+company_count = Company.count
+
+(MAX_COMPANIES - company_count).times do
+  company = FactoryGirl.create(:company)
+  employee_count = User.where(company: company).count
+  check_point_count = CheckPoint.where(company: company).count
+
+  FactoryGirl.create_list(:user, MAX_EMPLOYEES - employee_count, company: company)
+
+  (MAX_CHECK_POINTS - check_point_count).times do
+    check_point = FactoryGirl.create(:check_point, company: company)
+    question_count = Question.where(check_point: check_point).count
+
+    FactoryGirl.create_list(:question, MAX_QUESTIONS - question_count, check_point: check_point)
+  end
+
+  mentee_count = Mentee.where(company: company).count
+  FactoryGirl.create_list(:mentee, MAX_MENTEES - mentee_count, company: company)
+end
