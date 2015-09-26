def8a6953d351376eb5a8afee53de529981ffaab
Phuc Dam <ngocphuc@gmail.com>
2015-09-26 22:26:08 +0700

    check_points#restart

--- spec/controllers/check_points_controller_spec.rb
+++ spec/controllers/check_points_controller_spec.rb
@@ -58,7 +58,7 @@ RSpec.describe CheckPointsController do
         }.to change { mentee.check_points.count }.by(1)
       end
 
-      it 'redirects to' do
+      it 'redirects to :index' do
         post :create, mentee_id: mentee.id, check_point: params
 
         is_expected.to redirect_to(mentee_check_points_path)
@@ -135,69 +135,21 @@ RSpec.describe CheckPointsController do
     end
   end
 
-  describe '#question' do
-    let(:mentorship) { create(:mentorship, mentee: mentee, user: mentor) }
-    let(:mentee) { create(:mentee) }
-    let(:check_point) { create(:check_point, mentee: mentee) }
-    let!(:question) { create(:question, check_point: check_point) }
-
-    it 'renders correct data' do
-      get :question, mentee_id: mentee.id, id: check_point.id
-
-      expect(response).to be_success
-      expect(assigns[:question]).to eq question
-      expect(assigns[:answer]).to be_new_record
-      expect(assigns[:answer].question_id).to eq question.id
-      expect(assigns[:answer].user_id).to eq mentor.id
-    end
-  end
-
-  describe '#answer' do
-    let(:mentorship) { create(:mentorship, mentee: mentee, user: mentor) }
-    let(:mentee) { create(:mentee) }
-    let(:check_point) { create(:check_point, mentee: mentee) }
-    let!(:question) { create(:question, check_point: check_point) }
-    let(:note) { 'note' }
-    let(:answer_params) { {
-      question_id: question.id,
-      answer_value: answer_value,
-      note: note
-    } }
-
-    context 'when answer can be created' do
-      let(:answer_value) { 1 }
-
-      it 'creates answer' do
-        expect {
-          post :answer, answer: answer_params, id: check_point.id, mentee_id: mentee.id
-        }.to change { question.reload.answer }.from(nil)
-
-        expect(assigns[:answer].note).to eq note
-        expect(assigns[:answer].question_id).to eq question.id
-        expect(assigns[:answer].user_id).to eq mentor.id
-        expect(response).to redirect_to(dashboard_path)
-      end
+  describe 'POST finish' do
+    let(:mocked_service) { double }
+    before do
+      allow(mocked_service).to receive(:finish!)
+      allow(CheckPointService).to receive(:new).and_return(mocked_service)
     end
 
-    context 'when answer can not be created' do
-      let(:answer_value) { nil }
-
-      it 'renders error' do
-        expect {
-          post :answer, answer: answer_params, id: check_point.id, mentee_id: mentee.id
-        }.not_to change { question.reload.answer }
-
-        expect(response).to render_template(:question)
-        expect(assigns[:question]).to eq question
-      end
+    it 'finishes current check_point and starts new one' do
+      expect(mocked_service).to receive(:finish!)
+      post :finish, mentee_id: mentee.id, id: check_point.id
     end
 
-    it 'renders correct data' do
-      get :question, mentee_id: mentee.id, id: check_point.id
-
-      expect(assigns[:answer]).to be_new_record
-      expect(assigns[:answer].question_id).to eq question.id
-      expect(assigns[:answer].user_id).to eq mentor.id
+    it 'redirects to :index' do
+      post :finish, mentee_id: mentee.id, id: check_point.id
+      is_expected.to redirect_to(mentee_check_points_path)
     end
   end
 end
