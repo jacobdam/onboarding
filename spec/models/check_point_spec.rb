3e0531dd298848f01ecd2d8c1def2f3078954381
Tan Duong <dntan09@gmail.com>
2015-09-26 19:13:27 +0700

    Add more tests

--- spec/models/check_point_spec.rb
+++ spec/models/check_point_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe CheckPoint, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#should_have_started' do
+    let(:check_point) { FactoryGirl.create(:check_point, start_date: start_date) }
+
+    context 'when it start date have past today' do
+      let(:start_date) { 2.days.ago.to_date }
+
+      it 'returns true' do
+        expect(check_point.should_have_started).to be_truthy
+      end
+    end
+
+    context 'when it start date have NOT past today' do
+      let(:start_date) { 2.days.from_now.to_date }
+
+      it 'returns false' do
+        expect(check_point.should_have_started).to be_falsey
+      end
+    end
+  end
 end
