91a26e27361247b20ff6af4f49f01dbc45604580
Hieu Nguyen <hieuk09@gmail.com>
2015-09-26 11:48:39 +0700

    [f] Add new mentors to site

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,4 +1,24 @@
 class MentorsController < ApplicationController
   def new
+    @mentor = User.new(company: current_company, is_admin: false)
+  end
+
+  def create
+    @mentor = User.create(mentor_params)
+    if @mentor.valid?
+      redirect_to mentor_path(id: @mentor.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @mentor = current_company.users.find(params[:id])
+  end
+
+  private
+
+  def mentor_params
+    params.require(:user).permit(:email, :full_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
 
-  resources :mentors, only: [:new]
+  resources :mentors, only: [:new, :show, :create]
 end
