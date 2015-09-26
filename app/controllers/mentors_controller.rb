ad7de6174879e976298c10b815b38b5a5b060d56
Hieu Nguyen <hieuk09@gmail.com>
2015-09-26 12:11:30 +0700

    [f] Index page for mentors

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,4 +1,8 @@
 class MentorsController < ApplicationController
+  def index
+    @mentors = current_company.users.where(is_admin: false)
+  end
+
   def show
     @mentor = current_company.users.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'homes#index'
   resource :company, only: %i{new create edit update}
   resource :dashboard, only: %i{show}
-  resources :mentors, only: [:show]
+  resources :mentors, only: [:show, :index]
 end
