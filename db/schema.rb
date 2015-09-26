3ff803c0f72a1479e511390be4bd38907a3169c8
Tan Duong <dntan09@gmail.com>
2015-09-26 11:14:13 +0700

    Add company to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926034749) do
+ActiveRecord::Schema.define(version: 20150926041322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,8 +85,10 @@ ActiveRecord::Schema.define(version: 20150926034749) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
+    t.integer  "company_id"
   end
 
+  add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
@@ -96,4 +98,5 @@ ActiveRecord::Schema.define(version: 20150926034749) do
   add_foreign_key "mentees", "companies"
   add_foreign_key "questions", "check_points"
   add_foreign_key "questions", "question_templates"
+  add_foreign_key "users", "companies"
 end
