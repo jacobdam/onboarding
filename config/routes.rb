62019055b5b726650897f7c937057c3ef9d15e68
Phuc Dam <ngocphuc@gmail.com>
2015-09-27 09:20:56 +0700

    Restyle homepage & login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { invitations: 'users/invitations' }
-  root to: 'homes#index'
+  root to: 'dashboards#show'
   resource :company, only: %i{new create edit update}
   resource :dashboard, only: %i{show}
   resources :mentors, only: %i(index show)
